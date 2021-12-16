537bb92a1809dda113df06e425a3b155bbd6d201
Daniel <dnlggr@pm.me>
2021-12-16 13:58:15 +0100

    Update ignore script

--- src/boostagrams/ignore.sh
+++ src/boostagrams/ignore.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-git rm -r --cached $1
+# Remove and ignore specific custom records.
+#
+# Usage: ./ignore.sh custom_records/custom_record_NNNNN.json
+#
+# Todo: We could probably easily automate this via a GitHub workflow that
+# that is triggered by comments on the "Update Boostagrams" PR itself.
+
+FILE_DIR="$(cd -- "$(dirname "$1")" && pwd)"
+FILE="$(basename "$1")"
+FILE_FULL_PATH="$FILE_DIR/$FILE"
+
+git rm -r --cached $FILE_FULL_PATH
+echo "custom_records/$FILE" >> .gitignore
 git add .
-echo $1 >> .gitignore
-git commit -m "Remove $1"
+git commit -m "Remove $FILE"
 git push
 
-echo "Removed and added to gitignore: $1"
+echo -e "\nRemoved and added to gitignore: $FILE\n"
