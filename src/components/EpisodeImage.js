ceb9bfa7352870bc9ef3428f3700efbf2f6bc7b6
theborakompanioni <theborakompanioni+github@gmail.com>
2022-02-01 22:50:15 +0100

    fix: podcast image position

--- src/components/EpisodeImage.js
+++ src/components/EpisodeImage.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+const Picture = styled.picture`
+  display: flex;
+`
+
 const Image = styled.img`
   border-radius: 0.7rem;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
@@ -13,11 +17,11 @@ const Image = styled.img`
 
 const EpisodeImage = ({ episode, width, widthLarge }) => {
   return (
-    <picture>
+    <Picture>
       {episode.thumbnails?.webp && <source srcSet={episode.thumbnails.webp} type="image/webp" />}
       {episode.thumbnails?.jpg && <source srcSet={episode.thumbnails.jpg} type="image/jpeg" />}
       <Image src={episode.thumbnailFallback} width={width} widthLarge={widthLarge} alt="Episode Cover" />
-    </picture>
+    </Picture>
   )
 }
 
