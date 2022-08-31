2cfbfc227490564bafa9711a7d1641f434e77afa
cnix123 <111755602+cnix123@users.noreply.github.com>
2022-08-31 12:27:02 +0200

    fix: try to install yq

--- scripts/thumbnails.sh
+++ scripts/thumbnails.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
+set -e
+
+rm -rf /tmp/ctl-thumbnails
+mkdir -p /tmp/ctl-thumbnails
+
+if ! command -v wget &> /dev/null; then
+  echo "This script needs 'wget' to run. Consider installing it."
+  exit 1
+fi
+
+if ! command -v jq &> /dev/null; then
+    echo "This script needs 'jq' to run. Consider installing it."
+    exit 1
+fi
+
+if ! command -v convert &> /dev/null; then
+    echo "This script needs 'imagemagick' to run. Consider installing it."
+    exit 1
+fi
+
+if ! command -v yq &> /dev/null; then
+  if [ "$CI" = "true" ]; then
+    wget -q https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 -O yq && chmod +x yq
+  else
+    echo "This script needs 'yq' to run. Consider installing it."
+    exit 1
+  fi
+fi
+
+yq_command() {
+  if [ "$CI" = "true" ]; then
+    echo "./yq"
+  else
+    echo "yq"
+  fi
+}
+
 episodes=$(
   curl -sS https://closing-the-loop.github.io/feed.xml \
-    | yq -p xml -o json \
+    | $(yq_command) -p xml -o json \
     | jq '
         .rss.channel.item
           | .[]
@@ -18,9 +55,6 @@ find_command() {
   fi
 }
 
-rm -rf /tmp/ctl-thumbnails
-mkdir -p /tmp/ctl-thumbnails
-
 echo $episodes | jq --compact-output | while read episode ; do
   e=$(echo $episode | jq '.episode' | tr -d '"')
   s=$(echo $episode | jq '.season' | tr -d '"')
@@ -29,6 +63,7 @@ echo $episodes | jq --compact-output | while read episode ; do
   if [ -z $($(find_command) "public/assets/podcast/thumbnails" -maxdepth 1 -name "s${s}e${e}_*x*.jpg" -printf 1 -quit) ] || \
      [ -z $($(find_command) "public/assets/podcast/thumbnails" -maxdepth 1 -name "s${s}e${e}_*x*.webp" -printf 1 -quit) ]
   then
+    echo "Generating thumbnails for S${s}, E${e}..."
     wget -q -O "/tmp/ctl-thumbnails/s${s}e${e}_original.jpg" $i
     convert "/tmp/ctl-thumbnails/s${s}e${e}_original.jpg" -resize 400x400 -quality 90 "public/assets/podcast/thumbnails/s${s}e${e}_400x400.jpg"
     convert "/tmp/ctl-thumbnails/s${s}e${e}_original.jpg" -resize 400x400 -quality 90 "public/assets/podcast/thumbnails/s${s}e${e}_400x400.webp"
